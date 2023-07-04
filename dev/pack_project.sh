c295cc1bccecf8799ad792da522cf2d0d89c6d77
Marc Bohler <mbohler@vigilantsys.com>
2023-07-04 03:28:13 -0400

    Improved packaging script

--- dev/pack_project.sh
+++ dev/pack_project.sh
@@ -2,6 +2,8 @@
 
 cd ..
 git checkout student
-tar --exclude='.git' --exclude='.gitignore' -czvf ../vehicle_hacking_training.tar.gz ./
+cd ..
+tar --exclude='can_train/.git' --exclude='can_train/.gitignore' -czvf vehicle_hacking_training.tar.gz can_train/
+cd can_train
 git checkout main
 cd /home/kali/Documents/can_train
